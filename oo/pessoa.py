865c6cae1cc585f3f9a0b031628a934a427f7078
roderleimolao <roderleimolao@gmail.com>
2019-07-07 15:30:34 -0300

    Criado o método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,10 @@
 class Pessoa:  # classe sempre se inicia com  letra maiuscula
-    pass
+    # metodo é uma função que pertence a uma classe e portanto, sempre está conectada a um objeto.
+    def cumprimentar(self):
+        return f'Olá ! {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
